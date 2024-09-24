68235d07b157a6d7eea30778bf63d2d72dc75f81
mathildemong <mmonguillon@gmail.com>
2024-09-24 18:26:27 +0200

    logo enleve

--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
 import NavBar from '../NavBar/Navbar'
-import Reactimg from '../../assets/img/React.png';
- import phpimg from '../../assets/img/php.png';
-import htmlimg from '../../assets/img/html.png';
-import cssimg from '../../assets/img/css.png';
-// import jsimg from '../../assets/img/square-js.svg';
- import nodeimg from '../../assets/img/node.png';
-// import wordpressimg from '../../assets/img/wordpress.svg';
-// import background from "../../assets/img/fond.png";
 import '../aboutme/aboutme.css';
 import cv from '../../assets/img/cv.pdf';
 
@@ -29,17 +21,7 @@ function Aboutme() {
        <NavBar/>
        <div className="Text"><p>Voici les différentes technologies que j'ai pu utiliser durant mon parcours,  
         <a href={cv} target = "cv">téléchargez mon CV.</a></p></div>
-       <div className="logo"><div className="img">
-       <div className="img1"><img style={{ width: "100%", height: "30%" }} src={Reactimg} alt="React img" /></div>
-      <div className="img3"><img style={{ width: "100%", height: "30%" }} src={cssimg} alt="css img" /></div>
-        {/* <div className="img4"><img style={{ width: "100%", height: "50%" }} src={jsimg} alt="js img" /></div>
-       <div className="img5"><img style={{ width: "100%", height: "50%" }} src={wordpressimg} alt="wordpress img" /></div> */}
-         <div className="img6"><img style={{ width: "100%", height: "30%" }} src={nodeimg} alt="node img" /></div>
-      <div className="img6"><img style={{ width: "100%", height: "30%"}} src={htmlimg} alt="html img" /></div>
-       <div className="img2"><img style={{ width: "100%", height: "25%" }} src={phpimg} alt="php img" /></div> 
-       </div>
-                 
-            </div>
+     
             <div class="wavesA">
         <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
